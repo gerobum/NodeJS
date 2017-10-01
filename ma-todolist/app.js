a25501487aeb14788fad049fadd2cdb5751fc20a
Yvan <gerobum@gmail.com>
2017-10-01 22:02:59 +0200

    TP : todolist partagée

--- ma-todolist/app.js
+++ ma-todolist/app.js
@@ -1,67 +1,78 @@
 
 
 var app = require('express')(),
-    session = require('cookie-session'),
-    server = require('http').createServer(app),
-    bodyParser = require('body-parser'),
-    urlencodedParser = bodyParser.urlencoded({ extended: false }),
-    io = require('socket.io').listen(server),
-    ent = require('ent'), // Permet de bloquer les caractères HTML (sécurité équivalente à htmlentities en PHP)
-    fs = require('fs');
+        session = require('cookie-session'),
+        server = require('http').createServer(app),
+        bodyParser = require('body-parser'),
+        urlencodedParser = bodyParser.urlencoded({extended: false}),
+        io = require('socket.io').listen(server),
+        ent = require('ent'), // Permet de bloquer les caractères HTML (sécurité équivalente à htmlentities en PHP)
+        fs = require('fs');
 
 
 /* On utilise les sessions */
 app.use(session({secret: 'todotopsecret'}))
 
 
-/* S'il n'y a pas de todolist dans la session,
-on en crée une vide sous forme d'array avant la suite */
-.use(function(req, res, next){
-    if (typeof(req.session.todolist) === 'undefined') {
-        req.session.todolist = [];
-    }
-    next();
-})
+        /* S'il n'y a pas de todolist dans la session,
+         on en crée une vide sous forme d'array avant la suite */
+        .use(function (req, res, next) {
+            if (typeof (req.session.todolist) === 'undefined') {
+                req.session.todolist = [];
+            }
+            next();
+        })
 
-/* On affiche la todolist et le formulaire */
-.get('/todo', function(req, res) { 
-    res.render('todo.ejs', {todolist: req.session.todolist});
-})
+        /* On affiche la todolist et le formulaire */
+        .get('/todo', function (req, res) {
+            res.render('todo.ejs', {todolist: req.session.todolist});
+        })
 
-/* On ajoute un élément à la todolist */
-.post('/todo/ajouter/', urlencodedParser, function(req, res) {
-    if (req.body.newtodo !== '') {
-        req.session.todolist.push(req.body.newtodo);
-    }
-    res.redirect('/todo');
-})
+        /* On ajoute un élément à la todolist */
+        .post('/todo/ajouter/', urlencodedParser, function (req, res) {
+            if (req.body.newtodo !== '') {
+                req.session.todolist.push(req.body.newtodo);
+            }
+            res.redirect('/todo');
+        })
 
-/* Supprime un élément de la todolist */
-.get('/todo/supprimer/:id', function(req, res) {
-    if (req.params.id !== '') {
-        req.session.todolist.splice(req.params.id, 1);
-    }
-    res.redirect('/todo');
-})
+        /* Supprime un élément de la todolist */
+        .get('/todo/supprimer/:id', function (req, res) {
+            if (req.params.id !== '') {
+                req.session.todolist.splice(req.params.id, 1);
+            }
+            res.redirect('/todo');
+        })
 
-/* On redirige vers la todolist si la page demandée n'est pas trouvée */
-.use(function(req, res, next){
-    res.redirect('/todo');
-});
+        .get('/', function (req, res) {
+            res.sendfile(__dirname + '/index.html');
+        })
+
+        /* On redirige vers la todolist si la page demandée n'est pas trouvée */
+        .use(function (req, res, next) {
+            res.redirect('/todo');
+        });
 
 io.sockets.on('connection', function (socket, pseudo) {
-    // Dès qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes
-    socket.on('nouveau_client', function(pseudo) {
+    // Dès qu'on nous donne un pseudo, on le stocke en variable de session et 
+    // on informe les autres personnes
+    socket.on('nouveau_client', function (pseudo) {
         pseudo = ent.encode(pseudo);
         socket.pseudo = pseudo;
         socket.broadcast.emit('nouveau_client', pseudo);
     });
 
-    // Dès qu'on reçoit un message, on récupère le pseudo de son auteur et on le transmet aux autres personnes
+    // Dès qu'on reçoit un message, on récupère le pseudo de son auteur et 
+    // on le transmet aux autres personnes
     socket.on('message', function (message) {
         message = ent.encode(message);
         socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message});
-    }); 
+    });
+
+    socket.on('add', function (message) {
+        message = ent.encode(message);
+        console.log(message);
+    });
 });
 
 app.listen(8080);   
